b5239b50a98f523b980218d4e8894d4c72d922ae
Rivenshell Salomon <Rivenshell111@gmail.com>
2023-05-08 01:38:40 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,57 @@
+import * as THREE from "three"
+
+//canvas
+const canvas = document.querySelector("canvas.webgl")
+
+const scene = new THREE.Scene()
+
+console.log("what do you want from me?")
+
+// Objects
+// const geometry = new THREE.BoxGeometry(1, 1, 1)
+// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+// const mesh = new THREE.Mesh(geometry, material)
+// scene.add(mesh)
+
+// mesh.scale.x = 2
+// mesh.scale.y = 0.5
+// mesh.scale.z = 0.5
+// mesh.scale.set(2, 0.5, 0.5)
+
+//position
+// mesh.position.set(1, 0.5, 0.5)
+
+// mesh.rotation.reorder("YXZ")
+// mesh.rotation.y = 0.4 * Math.PI
+
+//axis helper
+const axesHelper = new THREE.AxesHelper(1)
+scene.add(axesHelper)
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: 800,
+  height: 600,
+}
+
+/**
+ * Camera
+ */
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+camera.position.x = 1
+camera.position.y = 1
+scene.add(camera)
+
+// camera.lookAt(mesh.position)
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
